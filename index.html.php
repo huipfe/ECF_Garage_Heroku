603a369c9bf23d9cd141ff79fb41570e049fa1fa
Ilan Tervil <ilan.tervil@wanadoo.fr>
2023-06-09 18:06:34 +0200

    👌 IMPROVE: Navbar and Footer in progress

--- index.html.php
+++ index.html.php
@@ -18,7 +18,11 @@ crossorigin="anonymous">
 <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,600;1,700;1,800;1,900&family=Rajdhani:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
 <!-- Style du projet -->
-<link rel="stylesheet" href="/Assets/CSS/style.css">
+<link rel="stylesheet" href="Assets/CSS/style.css">
+
+<!-- Boostrap Icons -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
 </head>
 
 
@@ -26,49 +30,112 @@ crossorigin="anonymous">
 
 
 <header>
-<nav class="navbar navbar-expand-md bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand img-fluid" href="#"><img src="https://trello.com/1/cards/646b18745ed83e14926328a3/attachments/646b1dd3b07071c56d54391a/download/image.png" width="100" height="auto"
-    alt="Logo Garage"></a></img>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link text-danger active" aria-current="page" href="#">Accueil</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-danger" href="#">Nos service</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-danger" href="#">Nos partenaires</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-danger" href="#">Nos produits</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-danger" href="#">A propos</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-danger" href="#">Contact</a>
-        </li>
-      </ul>
-
-    </div>
-  </div>
-</nav>
+    <nav class="navbar navbar-expand-md bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand img-fluid" href="#">
+                <img src="https://trello.com/1/cards/646b18745ed83e14926328a3/attachments/646b1dd3b07071c56d54391a/download/image.png" width="100" height="auto"
+            alt="Logo Garage">
+                </img>
+            </a>
+        
+        
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"           aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon text-danger"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+        
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Nos service</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Nos partenaires</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Nos produits</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">A propos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+        
+            </div>
+        </div>
+    </nav>
 </header>
 
 <main>
 
 </main>
 
-<footer>
-    <!-- place footer here -->
+<footer class="">
+<div class="container">
+    <div class="row">
+        <div class="col-md-3">
+        <h4>Nos horaires</h4>
+        <p>Horaires d'ouverture et de fermeture :</p>
+        <p>Lun  : 08h45 - 12h00/14h00 - 18h00</p>
+        <p>Mar : 08h45 - 12h00/14h00 - 18h00</p>
+        <p>Mer : 08h45 - 12h00/14h00 - 18h00</p>
+        <p>Jeu  : 08h45 - 12h00/14h00 - 18h00</p>
+        <p>Ven  : 08h45 - 12h00/14h00 - 18h00</p>
+        <p>Sam : 08h45 - 12h00</p>
+        <p>Dim  : Fermé</p>
+
+    </div>
+    <div class="col-md-3">
+        <h4>Liens rapides</h4>
+        <p>FAQ</p>
+        <p>Politique de confidentialité</p>
+        <p>Conditions d’utilisations</p>
+    </div>
+    <div class="col-md-3">
+        <h4>Nos expertises</h4>
+        <p>INSTALLATION DES CAMERAS</p>
+        <p>INSTALLATION DES GPS</p>
+        <p>INSTALLATION ELECTRIQUES</p>
+        <p>CAMERAS DE RECUL</p>
+        <p>CONTROLE D’ACCES</p>
+        <p>ALARMES ANTI-INCENDIE</p>
+        <p>CAPTEURS BIOMETRIQUES</p>
+        <p>ALARMES ANTI-INTRUSION</p>
+        </div>
+    <div class="col-md-3">
+    <h4>Contact</h4>
+        <p><i class="bi bi-geo-alt-fill"></i> 123 Rue du Garage, 75000 Ville</p>
+        <p><i class="bi bi-telephone-fill"></i> +1 234 567 890</p>
+        <p><i class="bi bi-envelope-fill"></i> contact@example.com</p>
+    <h4>Suivez nous sur :</h4>
+        <ul class="list-inline">
+            <li class="list-inline-item"><i class="bi bi-whatsapp"></i></li>
+            <li class="list-inline-item"><i class="bi bi-facebook"></i></li>
+            <li class="list-inline-item"><i class="bi bi-instagram"></i></li>
+            <li class="list-inline-item"><i class="bi bi-twitter"></i></li>
+        </ul>
+    </div>
 </footer>
 
+
+    <div class="container">
+<div class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 ">
+
+    <a href="#" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-autolink-body-emphasistext-decoration-none">
+        <img src="https://trello.com/1/cards/646b18745ed83e14926328a3/attachments/646b1dd3b07071c56d54391a/download/image.png" width="150" height="auto"
+            alt="Logo Garage">
+        </img>
+    </a>
+    <p class="col-md-4 mb-0 text-body-secondary">Garage V.Parrot © 2022 tous droits reserves  </p>
+        <div class="col-md-3">
+            <i class="bi bi-question-circle"></i>
+        </div>
+    </div>
+</div>
 
 <!-- Bootstrap JavaScript Libraries -->
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"

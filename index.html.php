db11118acfc25d03f47d7cf149f93c8fa79346b6
Ilan Tervil <ilan.tervil@wanadoo.fr>
2023-06-12 16:03:24 +0200

    📦 NEW: BDD SQL done

--- index.html.php
+++ index.html.php
@@ -5,6 +5,8 @@ require_once 'src/Views/templates/Header.html.php';
 ?>
 
 <main>
+    
+    <!-- Titre d'intro de la homepage -->
     <div class="container-fluid p-0 text-light position-relative">
         <h1 class="position-absolute top-0 start-0 m-3">
         <span style="font-weight: 700;">Le Garage V. Parrot,</span>
@@ -13,21 +15,32 @@ require_once 'src/Views/templates/Header.html.php';
         <span style="font-weight: 600;">Louez</span>
         <span style="font-weight: 300;">différemment...</span>
         </h1>
+
+        <!-- Image de fond, version desktop & mobile -->
         <img class="img-fluid d-lg-none" width="100%" height="auto" src="Assets/images/Fond_Voiture_homepage_mobile.jpg"
         alt="Fond_Voiture_homepage_mobile">
         </img>
         <img class="img-fluid d-none d-lg-block" width="100%" height="auto" src="Assets/images/Fond_Voiture_homepage_desktop.png"
         alt="Fond_Voiture_homepage_desktop">
         </img>
-            <h2 class="position-absolute top-50 start-50 translate-middle">Cliquez ici, pour en savoir plus</h2>
+
+        <!-- Bouton qui envois sur la page "à propos" -->
+        <div>
             <a href="#" class="col-md-4 d-flex align-items-center justify-content-start mb-3 mb-md-0
-        me-md-autolink-body-emphasistext-decoration-none position-absolute top-50 start-50 translate-middle">
-
-            <img src="Assets/images/Bouton_Voiture.png" width="100" height="auto"
-                alt="Bouton Voiture">
-            </img>
-        </a>
-
+            me-md-autolink-body-emphasistext-decoration-none position-absolute bottom-30 top-50 
+            start-50 translate-middle">
+                <!-- Mobile Bouton -->
+                <img src="Assets/images/Bouton_Voiture_mobile.png" class="img-fluid d-lg-none" width="100"
+                height="auto" alt="Bouton Voiture">
+                <!-- Desktop Bouton -->
+                <img src="Assets/images/Bouton_Voiture_desktop.png" class="img-fluid d-none d-lg-block" width="300"
+                height="auto" alt="Bouton Voiture2">
+            </a>
+            <h2 class="position-absolute top-50 start-50 translate-middle">
+                <span>Cliquez ici,</span>
+                <span>pour en savoir plus</span>
+            </h2>
+        </div>
 
     </div>
 </main>

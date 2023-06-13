594ed140e3848eceb13620522ce3b82d497ab11b
Ilan Tervil <ilan.tervil@wanadoo.fr>
2023-06-13 10:15:57 +0200

    📦 NEW: Finalization of Button from homepage to a-propos page

--- index.html.php
+++ index.html.php
@@ -20,25 +20,28 @@ require_once 'src/Views/templates/Header.html.php';
         <img class="img-fluid d-lg-none" width="100%" height="auto" src="Assets/images/Fond_Voiture_homepage_mobile.jpg"
         alt="Fond_Voiture_homepage_mobile">
         </img>
-        <img class="img-fluid d-none d-lg-block" width="100%" height="auto" src="Assets/images/Fond_Voiture_homepage_desktop.png"
+        <img class="img-fluid d-none d-lg-block" width="100%" height="auto"
+        src="Assets/images/Fond_Voiture_homepage_desktop.png"
         alt="Fond_Voiture_homepage_desktop">
         </img>
 
         <!-- Bouton qui envois sur la page "à propos" -->
-        <div>
-            <a href="#" class="col-md-4 d-flex align-items-center justify-content-start mb-3 mb-md-0
-            me-md-autolink-body-emphasistext-decoration-none position-absolute bottom-30 top-50 
+        <div class="position-relative">
+            <h2 class="position-absolute bottom-100 start-50 translate-middle bg-dark text-center mt-n5">
+                <span class="">Cliquez ici,</span>
+                <br>
+            <a href="Testimonials.html.php" class="col-md-4 d-flex align-items-center justify-content-start mb-3 mb-md-0
+            me-md-autolink-body-emphasistext-decoration-none position-absolute bottom-30 top-50
             start-50 translate-middle">
                 <!-- Mobile Bouton -->
-                <img src="Assets/images/Bouton_Voiture_mobile.png" class="img-fluid d-lg-none" width="100"
-                height="auto" alt="Bouton Voiture">
+                <img src="Assets/images/Bouton_Voiture_mobile.png" class="img-fluid d-lg-none d-block"
+                width="60" height="auto" alt="Bouton Voiture">
                 <!-- Desktop Bouton -->
-                <img src="Assets/images/Bouton_Voiture_desktop.png" class="img-fluid d-none d-lg-block" width="300"
-                height="auto" alt="Bouton Voiture2">
+                <img src="Assets/images/Bouton_Voiture_desktop.png" class="img-fluid d-none d-lg-block"
+                width="300"height="auto" alt="Bouton Voiture2">
             </a>
-            <h2 class="position-absolute top-50 start-50 translate-middle">
-                <span>Cliquez ici,</span>
-                <span>pour en savoir plus</span>
+            <br>
+                <span class="">pour en savoir plus</span>
             </h2>
         </div>
 

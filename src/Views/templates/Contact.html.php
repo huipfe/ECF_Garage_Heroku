e74e0767dd2ef9523737c7dde37a50435e478db9
Ilan Tervil <ilan.tervil@wanadoo.fr>
2023-06-15 17:21:12 +0200

    👌 IMPROVE: Begin of contact top page

--- src/Views/templates/Contact.html.php
+++ src/Views/templates/Contact.html.php
@@ -1,15 +1,15 @@
 <!-- Contact page: Allow visitors to contact the garage via a form. -->
 <!-- Page de contact : US4. Permettre de contacter l'atelier -->
 
-<!-- US4. Permettre de contacter l'atelier Utilisateurs concernés: Visiteurs Il est important que les 
-visiteurs du site puissent facilement joindre le garage à tout moment, que ce soit par téléphone 
+<!-- US4. Permettre de contacter l'atelier Utilisateurs concernés: Visiteurs Il est important que les
+visiteurs du site puissent facilement joindre le garage à tout moment, que ce soit par téléphone
 ou en remplissant un formulaire de contact en ligne.
-Si un visiteur décide d'utiliser le formulaire, il devra alors fournir son nom, prénom, adresse 
+Si un visiteur décide d'utiliser le formulaire, il devra alors fournir son nom, prénom, adresse
 e-mail, numéro de téléphone et un message.
-De plus, toutes les informations de contact, formulaire compris, devront également être visibles 
+De plus, toutes les informations de contact, formulaire compris, devront également être visibles
 en bas de chaque annonce d’un véhicule d’occasion. Le sujet du formulaire sera alors
 Reproduction interdite ECF - Garage automobile Page 7 sur 12
-automatiquement ajusté en fonction du titre de l'annonce pour permettre une future communication 
+automatiquement ajusté en fonction du titre de l'annonce pour permettre une future communication
 plus efficace avec le garage. -->
 
 <?php 
@@ -19,13 +19,22 @@ require_once "Header.html.php";
 <main>
     
 <section>
-
     <div class="p-1 bg-danger text-white">
-        <h1>dcsdfsd</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quia laudantium quod quasi, esse at iusto eos. Omnis veritatis tenetur voluptate blanditiis beatae necessitatibus eveniet, atque iste a labore sapiente.</p>
-    </div>
+        
+        <h1>Discutons ensemble, à propos de choses qui nous
+            <span style="color: #D9777F;">passionnent</span> !
 
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="#"><i class="bi bi-whatsapp link-light fs-1"></i></a></li>
+                <li class="list-inline-item"><a href="#"><i class="bi bi-facebook link-light fs-1"></i></a></li>
+                <li class="list-inline-item"><a href="#"><i class="bi bi-instagram link-light fs-1"></i></a></li>
+                <li class="list-inline-item"><a href="#"><i class="bi bi-twitter link-light fs-1"></i></a></li>
+            </ul>
+        </h1>
+
+    </div>
 </section>
+
 
 </main>
 

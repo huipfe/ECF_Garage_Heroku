aa400c83ec06710f49e5ea9be4336765d3044bf4
Ilan Tervil <ilan.tervil@wanadoo.fr>
2023-06-23 15:35:53 +0200

    👌 IMPROVE: Responsive improved

--- index.html.php
+++ index.html.php
@@ -1,62 +1,29 @@
 <!-- Charge mes éléments sur l'index -->
 <!-- Charge la homepage, qui charge mes footer & headers -->
-
-<?php 
+<?php
 require_once 'src/Views/templates/Homepage.html.php';
 ?>
 
 
 
+<!-- On ajoute l'autoloader -->
+<?php
+use App\Autoloader;
+use App\Models\HomepageModel;
 
-<!-- Instancie le compte de profil_user-->
-<?php 
-require_once 'src/Models/Profil_User.php';
-require_once 'src/Models/Cars_details.php';
-require_once 'src/Models/Employe.php';
-require_once 'src/Models/Admin.php';
-
-// $profil_user1 = new Profil_User('Admin', 1);
-// $profil_user1->getNom('Ilan Tervil');
-// $profil_user1->getPassword('1234azerty');
-// var_dump($profil_user1);
-// $profil_user1->getID(0);
-
-// echo "<br>";
-
-// $profil_user2 = new Profil_User('employee', 2);
-// $profil_user2->getNom('Bidule Bidulant');
-// $profil_user2->getPassword('987425Lol');
-// var_dump($profil_user2);
-// $profil_user2->getID(0);
+require_once 'src/Autoloader.php';
+Autoloader::register();
 
 
-?>
+$model = new HomepageModel;
 
+// $HomepageModel = $model
+//     ->setTemoignage('Nouveau Temoignage')
+//     ->setIdImage(1);
 
-<!-- Instancie une voiture-->
-<?php 
+// $model->create($HomepageModel);
 
-// echo "<br>";
+// var_dump($HomepageModel);
 
-// $car_details = new Cars_details('Honda Accord LX', 'Used', 2018, 28000, 13000, 'voiture_1.jpg', 1);
-// var_dump($car_details);
-
-?>
-
-
-<!-- Instancie un employé-->
-<?php 
-
-// $compte3 = new Employe('employé', 3);
-// var_dump($compte3);
-
-// echo "<br>";
-?>
-
-<!-- Instancie un admin-->
-<?php 
-
-// $compte4 = new Admin('admin', 4);
-// var_dump($compte4);
 
 ?>
--- src/Views/templates/Used_cars_listing.html.php
+++ src/Views/templates/Used_cars_listing.html.php
@@ -22,208 +22,165 @@ require_once "Header.html.php";
 ?>
 
 <main>
-<div>
-    <h1 class="products">Nos Produits</h1>
-</div>
+    <div>
+        <h1 class="products">Nos Produits</h1>
+    </div>
 
 
 
 <!-- Mis en avant -->
-<div class="container-lg">
-
-    <!-- Barre de recherche utilisateur -->
-    <div class="col-lg-6">
-        <div class="input-group">
-
-            <input
-            type="text"
-            class="form-control m-2 p-2 rounded-5"
-            placeholder="Recherche ">
-
-        <div class="input-group-append">
-            <button
-            class="btn btn-light m-2 p-2"
-            type="button">
-                <i
-                    class="bi bi-search">
-                </i>
-            </button>
-        </div>
-
-        </input>
-
-    </div>
-</div>
-
-
-<!-- Barre de filtrage -->
-<section>
-    <div class="m-2 filtrage-bar">
-        <h5 class="m-2 p-2 filtrage-title">
-            Liste des voitures <br> d'occasions par filtre
-        </h5>
-        <div class="gray-bar d-flex align-items-center">
-            <div class="form-check ">
-                <input class="form-check-input"  type="checkbox" value="" id="checkbox1">
-                <label class="form-check-label cursor-pointer" for="checkbox1">
-                    <span class="checkbox-icon pe-none"> Filtre 1</span>
-                </label>
+    <div class="container-lg">
+    
+        <!-- Barre de recherche utilisateur -->
+        <div class="col-lg-6">
+            <div class="input-group">
+    
+                <input
+                type="text"
+                class="form-control m-2 p-2 rounded-5"
+                placeholder="Recherche ">
+    
+            <div class="input-group-append">
+                <button
+                class="btn btn-light m-2 p-2"
+                type="button">
+                    <i
+                        class="bi bi-search">
+                    </i>
+                </button>
             </div>
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="checkbox2">
-                <label class="form-check-label cursor-pointer" for="checkbox2">
-                    <span class="checkbox-icon pe-none">Filtre 2</span>
-                </label>
-            </div>
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="checkbox3">
-                <label class="form-check-label cursor-pointer" for="checkbox3">
-                    <span class="checkbox-icon pe-none">Filtre 3</span>
-                </label>
-            </div>
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="checkbox4">
-                <label class="form-check-label cursor-pointer" for="checkbox4">
-                    <span class="checkbox-icon pe-none">Filtre 4</span>
-                </label>
-            </div>
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="checkbox5">
-                <label class="form-check-label cursor-pointer" for="checkbox5">
-                    <span class="checkbox-icon pe-none">Filtre 5</span>
-                </label>
-            </div>
+    
+            </input>
+    
         </div>
     </div>
-</section>
-
-
-
-
-
-	<!-- Inventory - START -->
-	<div class="row" id="ads">
-
-    <!-- Category Card -->
-    <div class="col-12 col-lg-4">
-        <div class="card rounded">
-            <div class="card-image">
-                <span class="card-notify-badge">Low KMS</span>
-                <span class="card-notify-year">2018</span>
-                <img class="img-fluid"
-                src="/ECF_Garage/Assets/images/Used cars listing/voiture_1.jpg" width="440" height="262"
-                alt="Alternate Text" />
-            </div>
-            <div class="card-image-overlay m-auto">
-                <span class="card-detail-badge">Used</span>
-                <span class="card-detail-badge">$28,000.00</span>
-                <span class="card-detail-badge">13000 Kms</span>
-            </div>
-            <div class="card-body text-center">
-                <div class="ad-title m-auto">
-                    <h5>Honda Accord LX</h5>
+    
+    
+    <!-- Barre de filtrage - Mobile -->
+    <section>
+            <div class="m-2 filtrage-bar d-lg-none">
+                <h5 class="m-2 p-2 filtrage-title">
+                    Liste des voitures <br> d'occasions par filtre
+                </h5>
+                <div class="gray-bar d-flex align-items-center">
+                    <div class="form-check ">
+                        <input class="form-check-input"  type="checkbox" value="" id="checkbox1">
+                        <label class="form-check-label cursor-pointer" for="checkbox1">
+                            <span class="checkbox-icon pe-none"> Filtre 1</span>
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="checkbox2">
+                        <label class="form-check-label cursor-pointer" for="checkbox2">
+                            <span class="checkbox-icon pe-none">Filtre 2</span>
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="checkbox3">
+                        <label class="form-check-label cursor-pointer" for="checkbox3">
+                            <span class="checkbox-icon pe-none">Filtre 3</span>
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="checkbox4">
+                        <label class="form-check-label cursor-pointer" for="checkbox4">
+                            <span class="checkbox-icon pe-none">Filtre 4</span>
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="checkbox5">
+                        <label class="form-check-label cursor-pointer" for="checkbox5">
+                            <span class="checkbox-icon pe-none">Filtre 5</span>
+                        </label>
+                    </div>
                 </div>
-                <a class="ad-btn" href="#">View</a>
             </div>
-        </div>
-    </div>
-    <div class="col-12 col-lg-4">
-        <div class="card rounded">
-            <div class="card-image">
-                <span class="card-notify-badge">Fully-Loaded</span>
-                <span class="card-notify-year">2017</span>
-                <img class="img-fluid"
-                src="/ECF_Garage/Assets/images/Used cars listing/voiture_2.jpg" width="440" height="262"
-                alt="Alternate Text" />
-            </div>
-            <div class="card-image-overlay m-auto">
-                <span class="card-detail-badge">Used</span>
-                <span class="card-detail-badge">$28,000.00</span>
-                <span class="card-detail-badge">13000 Kms</span>
-            </div>
-            <div class="card-body text-center">
-                <div class="ad-title m-auto">
-                    <h5>Honda CIVIC HATCHBACK LS</h5>
+    </section>
+        
+    <!-- -->
+        
+    <div class="container">
+            <div class="row">
+        
+                    <div class="col-lg-4">
+                        <div class="card mb-3">
+                            <div class="row g-0">
+                            <div class="col-md-5 col-lg-12">
+                                <img src="/ECF_Garage/Assets/images/Used cars listing/voiture_1.jpg"
+                                alt="Image 1" class="card-img">
+                            </div>
+                            <div class="col-md-7 col-lg-12">
+                                <div class="card-body">
+                                    <h5 class="card-title">Nom de la voiture 1</h5>
+                                    <p class="card-text">Description de la voiture 1</p>
+                                    <p class="card-text">Kilométrage : 10 000 km</p>
+                                    <p class="card-text">Prix : 20 000 €</p>
+                                <div class="rating">
+                                    <i class="bi bi-star-fill"></i>
+                                    <i class="bi bi-star-fill"></i>
+                                    <i class="bi bi-star-fill"></i>
+                                    <i class="bi bi-star-fill"></i>
+                                    <i class="bi bi-star"></i>
+                                </div>
+                                    <a href="#" class="btn btn-danger">Détails</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <a class="ad-btn" href="#">View</a>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-12 col-lg-4">
-        <div class="card rounded">
-            <div class="card-image">
-                <span class="card-notify-badge">Price Reduced</span>
-                <span class="card-notify-year">2018</span>
-                <img class="img-fluid"
-                src="/ECF_Garage/Assets/images/Used cars listing/voiture_3.jpg" width="440" height="262"
-                alt="Alternate Text" />
-            </div>
-            <div class="card-image-overlay m-auto">
-                <span class="card-detail-badge">Used</span>
-                <span class="card-detail-badge">$22,000.00</span>
-                <span class="card-detail-badge">8000 Kms</span>
-            </div>
-            <div class="card-body text-center">
-                <div class="ad-title m-auto">
-                    <h5>Honda Accord Hybrid LT</h5>
+            
+                    <div class="col-lg-4">
+                        <div class="card mb-3">
+                            <div class="row g-0">
+                            <div class="col-md-5 col-lg-12">
+                                <img src="/ECF_Garage/Assets/images/Used cars listing/voiture_2.jpg"
+                                alt="Image 1" class="card-img">
+                            </div>
+                            <div class="col-md-7 col-lg-12">
+                                <div class="card-body">
+                                    <h5 class="card-title">Nom de la voiture 1</h5>
+                                    <p class="card-text">Description de la voiture 1</p>
+                                    <p class="card-text">Kilométrage : 10 000 km</p>
+                                    <p class="card-text">Prix : 20 000 €</p>
+                                <div class="rating">
+                                    <i class="bi bi-star-fill"></i>
+                                    <i class="bi bi-star-fill"></i>
+                                    <i class="bi bi-star-fill"></i>
+                                    <i class="bi bi-star-fill"></i>
+                                    <i class="bi bi-star"></i>
+                                </div>
+                                    <a href="#" class="btn btn-danger">Détails</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <a class="ad-btn" href="#">View</a>
-            </div>
-        </div>
-    </div>
-
-</div>
-</div>
-
-
-<!-- Les autres -->
-<!-- <div class="container">
-
-
-<div class="row heading">
-    <h2 class="sub-heading">Inventory</h2>
-</div>
-
-<div class="container-fluid" id="inventory">
-    <div class="row">
-        <div class="col-md-3">
-            <div class="card">
-
-                    <img class="img-fluid"
-                    src="/ECF_Garage/Assets/images/Used cars listing/voiture_4.jpg" width="400" height="auto"
-                    alt="Card image cap">
-                    <button class="btn view-btn">SEDAN</button>
-                    <div class="ribbon"><span>Popular</span></div>
                 
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="card">
-                <img class="img-fluid"
-                src="/ECF_Garage/Assets/images/Used cars listing/voiture_5.jpg" width="400" height="auto"
-                alt="Card image cap">
-                <button class="btn view-btn">SUV</button>
-                <div class="ribbon"><span>Popular</span></div>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="card">
-                <img class="img-fluid"
-                src="/ECF_Garage/Assets/images/Used cars listing/voiture_6.jpg" width="400" height="auto"
-                alt="Card image cap">
-                <button class="btn view-btn">TRUCK</button>
-                <div class="ribbon"><span>Popular</span></div>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="card">
-                <img class="img-fluid"
-                src="/ECF_Garage/Assets/images/Used cars listing/HATCHBACK.jpg" width="400" height="auto"
-                alt="Card image cap">
-                <button class="btn btn-danger view-btn">HATCHBACK</button>
-                <div class="ribbon">
-                    <span>Popular</span>
+                    <div class="col-lg-4">
+                        <div class="card mb-3">
+                            <div class="row g-0">
+                            <div class="col-md-5 col-lg-12">
+                                <img src="/ECF_Garage/Assets/images/Used cars listing/voiture_3.jpg"
+                                alt="Image 1" class="card-img">
+                            </div>
+                            <div class="col-md-7 col-lg-12">
+                                <div class="card-body">
+                                    <h5 class="card-title">Nom de la voiture 1</h5>
+                                    <p class="card-text">Description de la voiture 1</p>
+                                    <p class="card-text">Kilométrage : 10 000 km</p>
+                                    <p class="card-text">Prix : 20 000 €</p>
+                                <div class="rating">
+                                    <i class="bi bi-star-fill"></i>
+                                    <i class="bi bi-star-fill"></i>
+                                    <i class="bi bi-star-fill"></i>
+                                    <i class="bi bi-star-fill"></i>
+                                    <i class="bi bi-star"></i>
+                                </div>
+                                    <a href="#" class="btn btn-danger">Détails</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -231,49 +188,46 @@ require_once "Header.html.php";
     </div>
 
 
-    <div class="row mt-5">
-        <div class="col-md-3">
-            <div class="card">
-                <img class="img-fluid"
-                src="/ECF_Garage/Assets/images/Used cars listing/WAGON.jpg"width="400" height="auto"
-                alt="Card image cap">
-                <button class="btn view-btn">WAGON</button>
+        <!-- Barre de filtrage - LG (sidebar) -->
+    <section>
+            <div class="m-2 filtrage-bar d-none d-lg-flex">
+                <h5 class="m-2 p-2 filtrage-title">
+                    Liste des voitures <br> d'occasions par filtre
+                </h5>
+                <div class="gray-bar d-flex align-items-center">
+                    <div class="form-check ">
+                        <input class="form-check-input"  type="checkbox" value="" id="checkbox1">
+                        <label class="form-check-label cursor-pointer" for="checkbox1">
+                            <span class="checkbox-icon pe-none"> Filtre 1</span>
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="checkbox2">
+                        <label class="form-check-label cursor-pointer" for="checkbox2">
+                            <span class="checkbox-icon pe-none">Filtre 2</span>
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="checkbox3">
+                        <label class="form-check-label cursor-pointer" for="checkbox3">
+                            <span class="checkbox-icon pe-none">Filtre 3</span>
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="checkbox4">
+                        <label class="form-check-label cursor-pointer" for="checkbox4">
+                            <span class="checkbox-icon pe-none">Filtre 4</span>
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="checkbox5">
+                        <label class="form-check-label cursor-pointer" for="checkbox5">
+                            <span class="checkbox-icon pe-none">Filtre 5</span>
+                        </label>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="col-md-3">
-            <div class="card">
-                <img class="img-fluid"
-                src="/ECF_Garage/Assets/images/Used cars listing/COUPE.jpg"width="400" height="auto"
-                alt="Card image cap">
-                <button class="btn btn-danger view-btn">COUPE</button>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="card">
-                <img class="img-fluid"
-                src="/ECF_Garage/Assets/images/Used cars listing/CONVERTIBLE.jpg"width="400" height="auto"
-                alt="Card image cap">
-                <button class="btn btn-danger view-btn">CONVERTIBLE</button>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="card">
-                <img class="img-fluid"
-                src="/ECF_Garage/Assets/images/Used cars listing/MINIVAN.jpg"width="400" height="auto"
-                alt="Card image cap">
-                <button class="btn  view-btn">MINIVAN</button>
-            </div>
-        </div>
-
-    </div> -->
-
-
-
-</div>
-
-<!-- Inventory - END -->
-</div>
-
+    </section>
 
 </main>
 

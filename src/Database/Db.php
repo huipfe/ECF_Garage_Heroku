12683487894af3399d2a346eca7c1adc03324ab7
Ilan Tervil <ilan.tervil@wanadoo.fr>
2023-06-29 15:28:53 +0200

    📦 NEW: Init of DB with controller folder

---
+++ src/Database/Db.php
@@ -0,0 +1,60 @@
+<?php
+namespace App\Database;
+// 'DB.php' : contient l'alimentation de la base de données.
+// La création de l'administrateur a été crée à partir du fichier 'DB.php' 
+// (comme pour pour toutes les autres créations fictives pour alimenter la database).
+
+// On importe PDO
+use PDO;
+use PDOException;
+
+
+class Db extends PDO 
+{
+
+    // Instance unique de la classe
+    private static $instance;
+
+    // Information de connexion
+    private const DBHOST = 'localhost';
+    private const DBUSER = 'root';
+    private const DBPASS = '';
+    private const DBNAME = 'ecf_garage';
+
+    // Constructeur
+    public function __construct() 
+    {
+        // DSN de connexion
+        $dsn = 'mysql:dbname='.self::DBNAME.';host='.self::DBHOST;
+
+        // On appelle le constructeur de la classe PDO
+        try {
+            parent::__construct($dsn, self::DBUSER, self::DBPASS);
+
+            // On définit le jeu de caractères en UTF-8
+            $this->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, 'SET NAMES utf8');
+            // à chaque fois que je fais un fetch, je le fais avec un tableau assosciatif
+            $this->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            // Déclencher une exception en cas d'erreur
+            $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+
+    // Méthode de connexion
+    public static function getInstance() 
+    {   
+        // Si l'instance n'existe pas
+        if (self::$instance === null) {
+            // On crée une instance de la classe
+            self::$instance = new self(); 
+        }
+        // On retourne l'instance
+        return self::$instance;
+    }
+}
+
+
+?>
---
+++ src/Database/create_database.sql
@@ -0,0 +1,137 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `ecf_garage`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `catalogue/listes voitures`
+--
+
+CREATE TABLE `catalogue_de_voitures` (
+    `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Table Catalogue voiture';
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `horaires`
+--
+
+CREATE TABLE `horaires` (
+    `jour` date NOT NULL,
+    `heure_debut` varchar(50) NOT NULL,
+    `heure_fin` varchar(50) NOT NULL,
+    `id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Tables des horaires';
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `img`
+--
+
+CREATE TABLE `img` (
+  `image` varchar(255) NOT NULL,
+  `id_image` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id_image`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Tables des images';
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `information perso garage parrot`
+--
+
+CREATE TABLE `information_garage_parrot` (
+  `adress` varchar(50) NOT NULL,
+  `num` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Table information du site';
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `page d'accueil`
+--
+
+CREATE TABLE `page_d_accueil` (
+  `Temoignage` varchar(50) NOT NULL,
+  `id_image` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Contenu de la page d''Accueil';
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rôle`
+--
+
+CREATE TABLE `rôle` (
+  `name` varchar(50) NOT NULL,
+  `is_admin` tinyint(1) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Table Rôle, qui désigne l''admin ou l''employé';
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `témoignages (commentaire)`
+--
+
+CREATE TABLE `témoignages` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `contenu` varchar(50) NOT NULL,
+  `date` date NOT NULL,
+  `nom` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Tables des commentaires/témoignages';
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `passe_word` varchar(255) NOT NULL,
+  `login_status` tinyint(1) NOT NULL,
+  `nom_de_registre` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Table Utilisateur (Pour admin et employé)';
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `voiture`
+--
+
+CREATE TABLE `voiture` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_image` int NOT NULL,
+  `prix` float NOT NULL,
+  `année` int NOT NULL,
+  `kilometrage` int NOT NULL,
+  `marque` varchar(50) NOT NULL,
+  `modele` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Table de voiture, liée au catalogue';
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

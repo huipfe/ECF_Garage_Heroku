5ac75c4f24037ae02705d244b7db3a1112ca12f0
Ilan Tervil <ilan.tervil@wanadoo.fr>
2023-07-17 17:31:37 +0200

    📦 NEW: Page Apropos with SCSS and image

--- src/Views/templates/Services.html.php
+++ src/Views/templates/Services.html.php
@@ -1,18 +1,74 @@
-<!-- Services page: Present information about the different automotive repair services offered. -->
-<!-- Page d'accueil : US2. Présenter les services, US5. Recueillir les témoignages des clients -->
-<!-- US2. Présenter les services Utilisateurs concernés: Administrateur, Visiteurs Le site web doit 
-afficher de manière claire et concise les différents services de réparation automobile proposés par 
-le garage sur sa page d'accueil.
-De plus, l'administrateur doit avoir la possibilité de modifier ces informations directement à partir 
-de son espace d'administration. -->
-
 <?php
 require_once "Header.html.php";
 ?>
 
 <main>
     <div class="container">
-        <h1>Nos Service</h1>
+        <h1>Nos Services</h1>
+        <div class="row">
+
+
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <img src="chemin_vers_image1.jpg" class="card-img-top" alt="Service 1">
+                    <div class="card-body">
+                        <h5 class="card-title">Service 1</h5>
+                        <p class="card-text">Description du service 1</p>
+                        <p class="card-text">Prix: $XX</p>
+                        <p class="card-text">Durée estimée: XX heures</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <img src="chemin_vers_image2.jpg" class="card-img-top" alt="Service 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Service 2</h5>
+                        <p class="card-text">Description du service 2</p>
+                        <p class="card-text">Prix: $XX</p>
+                        <p class="card-text">Durée estimée: XX heures</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <img src="chemin_vers_image3.jpg" class="card-img-top" alt="Service 3">
+                    <div class="card-body">
+                        <h5 class="card-title">Service 3</h5>
+                        <p class="card-text">Description du service 3</p>
+                        <p class="card-text">Prix: $XX</p>
+                        <p class="card-text">Durée estimée: XX heures</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <img src="chemin_vers_image3.jpg" class="card-img-top" alt="Service 3">
+                    <div class="card-body">
+                        <h5 class="card-title">Service 4</h5>
+                        <p class="card-text">Description du service 4</p>
+                        <p class="card-text">Prix: $XX</p>
+                        <p class="card-text">Durée estimée: XX heures</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <img src="chemin_vers_image3.jpg" class="card-img-top" alt="Service 3">
+                    <div class="card-body">
+                        <h5 class="card-title">Service 5</h5>
+                        <p class="card-text">Description du service 5</p>
+                        <p class="card-text">Prix: $XX</p>
+                        <p class="card-text">Durée estimée: XX heures</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
 </main>
 
